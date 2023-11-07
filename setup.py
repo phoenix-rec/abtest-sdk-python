452b85ea5db0b7dbc729631ca85cfeb1cf0a8af1
lingfei <lingfei2014@xiaochuankeji.cn>
2023-11-07 18:52:54 +0800

    add err log

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='abtest-sdk',
-    version='1.0.4',  # 版本号
+    version='1.0.5',  # 版本号
     description='ab SDK for phoenix',  # 描述
     author='',
     author_email='',
